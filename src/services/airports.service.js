ab10ac0cebdc39361895bf80bc1580bd1c3659c0
Student1-41277392 <Student1-41277392@LODSPRODMCA.onmicrosoft.com>
2024-06-04 12:50:56 -0700

    test commit

--- src/services/airports.service.js
+++ src/services/airports.service.js
@@ -7,8 +7,9 @@ class AirportsService {
         this._airports = airports;
     }
 
+    // test
     getAll(){
-        return this._airports.filter(a => a.code).map(avoidEmptyCity);
+        return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a, b) => (a.city > b.city) ? 1 : -1);
     }
 
     getByCode(code) {
